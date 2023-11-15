eab20078321192228f0cff0e6abdeaa19c89b0a3
Jellz01 <jellzwellesley@gmail.com>
2023-11-15 15:12:04 +0000

    ya camviei

--- WellesleyJ_MendozaF_prueba/src/main/java/Modelos/DatosEstado.java
+++ WellesleyJ_MendozaF_prueba/src/main/java/Modelos/DatosEstado.java
@@ -8,9 +8,8 @@ package Modelos;
  *
  * @author jellz
  */
-public class DatosEstado implements Datos{
-    
-    
+
+public class DatosEstado implements Datos {
     private Datos datos;
 
     public DatosEstado(Datos datos) {
@@ -21,7 +20,25 @@ public class DatosEstado implements Datos{
     public void procesar() {
         datos.procesar();
         // Lógica para identificar estado
-        // ...
+        identificarEstado();
+    }
+
+    private void identificarEstado() {
+        // Lógica para verificar el estado de cada estudiante
+        // Suponiendo que las notas están en la posición 1, 2 y 3 de la matriz
+        for (String[] estudiante : estudiantes) {
+            int nota1 = Integer.parseInt(estudiante[1]);
+            int nota2 = Integer.parseInt(estudiante[2]);
+            int nota3 = Integer.parseInt(estudiante[3]);
+
+            int sumatoria = nota1 + nota2 + nota3;
+            
+            if (sumatoria < 70) {
+                System.out.println(estudiante[0] + " ha reprobado.");
+            } else {
+                System.out.println(estudiante[0] + " ha aprobado.");
+            }
+        }
     }
 }
 
--- WellesleyJ_MendozaF_prueba/src/main/java/Modelos/Inicio.java
+++ WellesleyJ_MendozaF_prueba/src/main/java/Modelos/Inicio.java
@@ -15,6 +15,9 @@ public class Inicio {
          String[][] estudiantes = {
                 {"Joseph", "80", "90", "75"},
                 {"Fabian", "70", "85", "92"},
+                
+                
+                el fabian es una perra
                 // ... Agrega más estudiantes según sea necesario
         };
 
